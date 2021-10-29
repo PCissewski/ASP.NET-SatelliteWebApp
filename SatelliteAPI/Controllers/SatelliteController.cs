7171fe48836c87626a1ca276af2a8842ec6e2c71
PCissewski <cissek99@gmail.com>
2021-10-29 15:48:31 +0200

    Initial version of a project

---
+++ SatelliteAPI/Controllers/SatelliteController.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstAPI.Controllers
+{
+    public class SatelliteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
